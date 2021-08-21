76c99c9bb8313a7106b75cd136ac6d2a2ec1d91f
DYSTAR14 <ydeepak141097@gmail.com>
2021-08-21 23:12:14 +0530

    Add: Add method to choose X or O

--- src/com/blz/tictactoe_game/TicTacToeGame.java
+++ src/com/blz/tictactoe_game/TicTacToeGame.java
@@ -1,6 +1,10 @@
 package com.blz.tictactoe_game;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
+	static final Scanner SC = new Scanner(System.in);
+	
 	public char[][] createTicTacToeBoard() {
 		char[][] ticTacToeBoard = { { '1', '|', '2', '|', '3' }, 
 				{ '-', '+', '-', '+', '-' },
@@ -10,9 +14,36 @@ public class TicTacToeGame {
 		return ticTacToeBoard;
 	}
 
+	public char choose_XO() {
+		System.out.println("Choose between X or O");
+		char letter = SC.next().charAt(0);
+		if (letter == 'x' || letter == 'X') {
+			letter = 'X';
+			return letter;
+		} else if (letter == 'o' || letter == 'O') {
+			letter = 'O';
+			return letter;
+		} else {
+			System.out.println("Invalid Input");
+			return 0;
+		}
+	}
+	
+	public void chooseXOUserComp(char[][] ticTacToeBoard) {
+		char symbolPlayer = 0;
+		char symbolComputer;
+		symbolPlayer = choose_XO();
+
+		if (symbolPlayer == 'X') {
+			symbolComputer = 'O';
+		} else if (symbolPlayer == 'O') {
+			symbolComputer = 'X';
+		}
+	}
+	
 	public static void main(String[] args) {
 		TicTacToeGame ticTacToeGame = new TicTacToeGame();
-		char[][] ticTacToeBoard = ticTacToeGame.createTicTacToeBoard();
-		
+		char[][] ticTacToeBoard = ticTacToeGame.createTicTacToeBoard();	
+		ticTacToeGame.chooseXOUserComp(ticTacToeBoard);
 	}
 }
