a65eb85d9a32e309501da3ef3996dcac5d10fb8e
DYSTAR14 <ydeepak141097@gmail.com>
2021-08-21 22:36:38 +0530

    add: Create New Board of TicTacToe

--- src/com/blz/tictactoe_game/TicTacToeGame.java
+++ src/com/blz/tictactoe_game/TicTacToeGame.java
@@ -1,7 +1,24 @@
 package com.blz.tictactoe_game;
 
 public class TicTacToeGame {
+	public char[][] createTicTacToeBoard() {
+		char[][] ticTacToeBoard = { { '1', '|', '2', '|', '3' }, 
+				{ '-', '+', '-', '+', '-' },
+				{ '4', '|', '5', '|', '6' }, 
+				{ '-', '+', '-', '+', '-' }, 
+				{ '7', '|', '8', '|', '9' } };
+		return ticTacToeBoard;
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome TicTacToe Game");
+		TicTacToeGame ticTacToeGame = new TicTacToeGame();
+		char[][] ticTacToeBoard = ticTacToeGame.createTicTacToeBoard();
+		System.out.println("Tic Tac Toe Game Board");
+		for (char[] row : ticTacToeBoard) {
+			for (char col : row) {
+				System.out.print(col + " ");
+			}
+			System.out.println();
+		}
 	}
 }
