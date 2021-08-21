d535a562868cc431788772c748863c7d2e6b0266
DYSTAR14 <ydeepak141097@gmail.com>
2021-08-21 22:38:42 +0530

    Refactor: Remove print statement

--- src/com/blz/tictactoe_game/TicTacToeGame.java
+++ src/com/blz/tictactoe_game/TicTacToeGame.java
@@ -13,12 +13,6 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		TicTacToeGame ticTacToeGame = new TicTacToeGame();
 		char[][] ticTacToeBoard = ticTacToeGame.createTicTacToeBoard();
-		System.out.println("Tic Tac Toe Game Board");
-		for (char[] row : ticTacToeBoard) {
-			for (char col : row) {
-				System.out.print(col + " ");
-			}
-			System.out.println();
-		}
+		
 	}
 }
